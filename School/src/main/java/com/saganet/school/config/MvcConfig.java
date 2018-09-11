443c2c8bbf936f88e1f47470bb9f4977d68fd722
Erick SaGa <esanchez@saganet.com.mx>
2018-09-11 10:53:49 -0500

    Se arregla configuracón de JSF
    Se activa el archivo faces-config para registrar Converters
    Se agrega modulo inicial para Alumnos

--- School/src/main/java/com/saganet/school/config/MvcConfig.java
+++ School/src/main/java/com/saganet/school/config/MvcConfig.java
@@ -1,8 +1,14 @@
 package com.saganet.school.config;
 
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -12,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 @ImportResource({"classpath:/META-INF/servlet-config.xml"})
+@ComponentScan("com.saganet.school")
 public class MvcConfig implements WebMvcConfigurer {
 	
 	@Bean
@@ -24,6 +31,13 @@ public class MvcConfig implements WebMvcConfigurer {
 		DispatcherServletRegistrationBean dispatcherServletRB = new DispatcherServletRegistrationBean(dispatcherServlet(), "/app/*");
 		dispatcherServletRB.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME);
 		return dispatcherServletRB;
+	}
+	
+	@Bean
+	public ServletRegistrationBean<Servlet> facesServlet() {
+		ServletRegistrationBean<Servlet> register = new ServletRegistrationBean<Servlet>(new FacesServlet(), "*.faces");
+		register.setLoadOnStartup(1);
+		return register;
 	}
 	
 }
