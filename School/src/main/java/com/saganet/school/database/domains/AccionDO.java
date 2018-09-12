f72fc91f18e8cd1acff6c3e1083c87f78993c6ab
Erick SaGa <esanchez@saganet.com.mx>
2018-09-12 13:55:14 -0500

    Se agregan catálogos de Géneros y Acciones

--- School/src/main/java/com/saganet/school/database/domains/AccionDO.java
+++ School/src/main/java/com/saganet/school/database/domains/AccionDO.java
@@ -1,7 +1,17 @@
 package com.saganet.school.database.domains;
 
 public enum AccionDO {
-	INSERT,
-	UPDATE,
-	DELETE;
+	INSERT("Insertar"),
+	UPDATE("Actualizar"),
+	DELETE("Borrar");
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	private final String nombre;
+
+	private AccionDO(String nombre) {
+		this.nombre = nombre;
+	}
 }
