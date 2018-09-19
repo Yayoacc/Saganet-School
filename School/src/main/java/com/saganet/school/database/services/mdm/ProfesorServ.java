0218af4d50edd1f300482572057c955effb9f5e8
darioph <darioph@darioph-Latitude-E6430s>
2018-09-19 10:41:57 -0500

    Se agrega clases y vistas correspondientes a Profesores.

---
+++ School/src/main/java/com/saganet/school/database/services/mdm/ProfesorServ.java
@@ -0,0 +1,42 @@
+package com.saganet.school.database.services.mdm;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saganet.school.database.daos.mdm.ProfesorDao;
+import com.saganet.school.database.entities.mdm.AlumnoEO;
+import com.saganet.school.database.entities.mdm.ProfesorEO;
+import com.saganet.school.utils.Modelo;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service("ProfesorServ")
+@Slf4j
+public class ProfesorServ {
+	
+	@Autowired
+	private ProfesorDao profesorDao;
+	
+	@Autowired
+	private DireccionServ direccionServ;
+	
+	public Modelo<ProfesorEO> modeloProf(){
+		List<ProfesorEO> listado;
+		listado=profesorDao.findAll();
+		
+		return new Modelo<>(listado);
+	}
+	
+	public ProfesorEO nuevo() {
+		return new ProfesorEO();
+	}
+	
+	public void guardar(ProfesorEO profesor) {
+		log.debug("Se guarda/actualiza profesor: {}", profesor);
+		direccionServ.guardar(profesor.getDireccion());
+		profesorDao.save(profesor);
+	}
+}
