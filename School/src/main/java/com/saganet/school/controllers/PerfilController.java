d5413c413fa12a986891bc751a5f1b17dd14f385
Yacc <yayoacc_96@hotmail.com>
2018-10-30 10:39:42 -0600

    Se agregó login a página con Spring Security

--- School/src/main/java/com/saganet/school/controllers/PerfilController.java
+++ School/src/main/java/com/saganet/school/controllers/PerfilController.java
@@ -3,13 +3,12 @@ package com.saganet.school.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/vc")
 public class PerfilController {
-	@RequestMapping("/perfil")
-	public String vista(String name, Model model) {
-		
-		return "perfil";
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
 	}
-}
+} 	 
